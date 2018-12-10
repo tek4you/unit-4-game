09c84212e324ae1260ed8a9bc21de33499a344d9
FrankV <frank@sabertechnology.com>
2018-12-10 13:02:47 -0500

    Restart with new strategy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,27 @@ function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+function assignGemValue() {
+    for (i = 0; i < ((crystal.gem).length); i++) {
+        var randNum = randomIntFromInterval(1, 12);
+        var targetGem = crystal.gem[i];
+        $(targetGem).attr("value", randNum);
+
+        var gemVal = $(targetGem).attr("#value");
+
+        console.log({
+            randNum: randNum,
+            currentCrystalGem: crystal.gem[i],
+            targetGem: targetGem,
+            gemVal: gemVal
+            // targetGemLength: (crystal.gem).length 
+        });
+        // $(crystal.[gem[i]]).attr("value", randNum);
+
+        
+        // console.log({
+        //     gemVal: gemVal
+        // })    
+    }
+}
