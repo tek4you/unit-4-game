abd06837a71622cefcb3d4d53fba8810eba5d49b
FrankV <frank@sabertechnology.com>
2018-12-14 20:08:10 -0500

    Almost final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,15 @@ function randomIntFromInterval(min,max)
 
 function initialize() {
 
-// var currentScore = 0;
 
 winScore = randomIntFromInterval(19, 120);
      console.log({
      winScore: winScore,
-    // currentScore: currentScore
+   
       });
       $("#placeWinScoreHere").text(winScore); 
 counter = 0;
     $(".score-display").text(counter);
+   
 return winScore;
     }
